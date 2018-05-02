266574614c980f2c67d07ff405f3f0ca18a5302d
bbayu123 <26726381+bbayu123@users.noreply.github.com>
2018-05-02 18:47:25 +0800

    New Particle, edited existing particles
    Added another particle type as Hell requested before, set AOE particles to have 4-degree separation, and fixed existing particles, by changing the input of Math.cos() and Math.sin() from a degree angle to a radian angle.

--- src/hell/supersoul/magic/util/ParticleUtil.java
+++ src/hell/supersoul/magic/util/ParticleUtil.java
@@ -7,29 +7,67 @@ import org.bukkit.entity.Player;
 
 public class ParticleUtil {
 
-    private static void reddust(World w, Double x, Double y, Double z, Double r, Double g, Double b) {
-        w.spawnParticle(Particle.REDSTONE, x, y, z, 0, r / 255.0, g / 255.0, b / 255.0);
+    private static void redDust(World w, Double x, Double y, Double z, Double r, Double g, Double b) {
+        /* 
+         * When red value == 0 then it will always be red no matter what the other colour values are,
+         * that's why if red == 0 set it to Float.MIN_VALUE
+         */
+        w.spawnParticle(Particle.REDSTONE, x, y, z, 0, r == 0 ? Float.MIN_VALUE : red / 255.0, g / 255.0, b / 255.0, 1);
     }
 
     public static void createAOEParticles(Player p, Double radius, Double angle, Double red, Double green, Double blue, Integer count) {
-        for(Integer i = 0; i <= 360; i++) {
-            Location loc = p.getLocation();
-            Double pointx = radius * Math.sin(i);
-            Double pointz = radius * Math.cos(i);
-            pointx += loc.getX();
-            pointz += loc.getZ();
-            reddust(p.getWorld(), pointx, loc.getY(), pointz, red, green, blue);
+        Location loc = p.getLocation().clone();
+        
+        // The ring with 4 degree separation
+        for(Integer i = 0; i <= 360; i += 4) {
+            Double pointx = loc.getX() + radius * Math.sin(Math.toRadians(i));
+            Double pointz = loc.getZ() + radius * Math.cos(Math.toRadians(i));
+            redDust(p.getWorld(), pointx, loc.getY(), pointz, red, green, blue);
         }
     }
 
     public static void createHelixParticles(World w, Double x, Double y, Double z, Double radius, Double angle, Double red, Double green, Double blue, Integer count) {
-        Double h1x = radius * Math.sin(angle);
-        Double h1z = radius * Math.cos(angle);
+        Double h1x = radius * Math.sin(Math.toRadians(angle));
+        Double h1z = radius * Math.cos(Math.toRadians(angle));
         h1x += x;
         h1z += z;
         for(Integer i = 0; i < count; i++) {
-            reddust(w, h1x, y, h1z, red, green, blue);
+            redDust(w, h1x, y, h1z, red, green, blue);
         }
     }
+    
+    public void createDefenseParticles(Player player, Double radius, Double angle, Double red, Double green, Double blue) {
+        // Hell asked me to make this, not sure what the use is for so I place it here for now
+		Location loc = player.getLocation().clone();
+		double yaw = loc.getYaw() < 0 ? loc.getYaw() + 360 : loc.getYaw();
+		Particle particle = Particle.REDSTONE;
+        
+		// The ring
+		for (double i = 0; i < 360; i += 5) {
+			// Removing part of the ring in front of the player.
+			if (yaw - angle / 2 >= 0 && yaw + angle / 2 < 360) {
+				if (i >= yaw - angle / 2 && i <= yaw + angle / 2) continue;
+			}
+			else if (yaw - angle / 2 < 0) {
+				if (i >= yaw - angle / 2 + 360 || i <= yaw + angle / 2) continue;
+			}
+			else if (yaw + angle / 2 >= 360) {
+				if (i >= yaw - angle / 2 || i <= yaw + angle / 2 - 360) continue;
+			}
+			double z = loc.getZ() + Math.cos(Math.toRadians(i)) * radius;
+			double x = loc.getX() - Math.sin(Math.toRadians(i)) * radius;
+            
+			// Spawning the ring.
+			for (double y = loc.getY() + 0.7; y <= loc.getY() + 1.7; y += 0.5)
+                redDust(p.getWorld(), x, y, z, red, green, blue)
+		}
+		// The line at the back
+		for (double y = loc.getY() + 0.7; y <= loc.getY() + 1.7; y += 0.1) {
+			double back = yaw < 180 ? yaw + 180 : yaw - 180;
+			double z = loc.getZ() + Math.cos(Math.toRadians(back)) * radius;
+			double x = loc.getX() - Math.sin(Math.toRadians(back)) * radius;
+			redDust(p.getWorld(), x, y, z, red, green, blue)
+		}
+	}
 
 }
