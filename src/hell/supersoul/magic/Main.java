bc7ba18459f6975ba6b1936402e509e0f6f69e94
SupersoulMC <taikongrenqiu@hotmail.com>
2018-05-03 22:44:07 +0800

    Removed the old combo system.

--- src/hell/supersoul/magic/Main.java
+++ src/hell/supersoul/magic/Main.java
@@ -5,10 +5,16 @@ import hell.supersoul.magic.core.RegularM;
 import hell.supersoul.magic.core.regular.Lightning;
 import hell.supersoul.magic.events.EventProcesser;
 import hell.supersoul.magic.util.InventoryPacketListener;
+
+import java.util.ArrayList;
+
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
